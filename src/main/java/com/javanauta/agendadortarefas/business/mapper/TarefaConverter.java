35c5cb3ef374c3750548af71f249f03ea736fa11
João <joaolucassantos.dev@gmail.com>
2024-09-27 17:42:54 -0300

    Adicionados metodos Get por email e por periodo

--- src/main/java/com/javanauta/agendadortarefas/business/mapper/TarefaConverter.java
+++ src/main/java/com/javanauta/agendadortarefas/business/mapper/TarefaConverter.java
@@ -6,12 +6,21 @@ import com.javanauta.agendadortarefas.infrastructure.entity.TarefasEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface TarefaConverter {
 
     @Mapping(source = "id", target = "id")
+    @Mapping(source = "dataEvento", target = "dataEvento")
+    @Mapping(source = "dataCriacao", target = "dataCriacao")
+
     TarefasEntity paraTarefaEntity(TarefasDTO dto);
 
     TarefasDTO paraTarefaDTO(TarefasEntity entity);
+
+    List<TarefasEntity> paraListasTarefasEntity(List<TarefasDTO> dtos);
+
+    List<TarefasDTO> paraListaTarefasDTO(List<TarefasEntity> entities);
 
 }
